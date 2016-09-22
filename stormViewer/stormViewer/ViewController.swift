4f4ff51b83aec32083dbf58e78586227be62772a
Andre Dias <andre.dias@concrete.com.br>
2016-09-22 18:14:32 -0300

    Adiciona arquivos do xcode contendo o projeto
    Adiciona base do codigo no view controller, lista as imagens contidas no projeto adciona em um array de strings

---
+++ stormViewer/stormViewer/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  stormViewer
+//
+//  Created by Andre Dias on 22/09/16.
+//  Copyright © 2016 Andre Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
