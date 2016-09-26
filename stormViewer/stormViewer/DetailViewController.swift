588ab4f530c0cecc6684decca6d56c1e51f71d0f
Andre Dias <andre.dias@concrete.com.br>
2016-09-26 14:51:35 -0300

    Adiciona funcionalidade de share para redes sociais com UIActivityViewController

--- stormViewer/stormViewer/DetailViewController.swift
+++ stormViewer/stormViewer/DetailViewController.swift
@@ -18,6 +18,8 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         title = selectedImage
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -32,6 +34,13 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    func shareTapped(){
+        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
